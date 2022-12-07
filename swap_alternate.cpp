d5f61a5e13f8c5aaaabcb8ae14a03548addb5d17
tanaySharma <Tanay Sharma@LAPTOP-JTQ66A27>
2022-12-07 12:02:56 +0530

    the program is enhanced

---
+++ swap_alternate.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+void swapAlternate(int arr[], int size)
+{
+    for (int i = 0; i < size-1; i=i+2)
+    {
+        if (i<size)
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+        
+    }
+    
+}
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+    cout<< " "<<arr[i];
+    }
+    
+}
+int main()
+{
+    int array[] = {2, 4, 5, 3, 8};
+    int size = sizeof(array) / sizeof(int);
+    printArray(array, size);
+    swapAlternate(array, size);
+    cout<< "the updated array is: <<endl;
+    printArray(array, size);
+
+
+    return 0;
+}
+
+
+
+
+
